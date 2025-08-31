f756a7e7b8fafecf95355e4bc48ee36bf897ad34
Anas <111919185+Cornuche92a@users.noreply.github.com>
2025-08-31 18:30:02 +0100

    Fix dependency conflicts and headers error

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,14 +1,9 @@
 import { getRequestConfig } from "next-intl/server";
-import { headers } from "next/headers";
 
 const AVAILABLE_LOCALES = ["ar", "de", "en", "fr", "lt", "nl", "sv", "es"];
 
-export default getRequestConfig(async () => {
-  let locale: string;
-  const [headersList] = await Promise.all([headers()]);
-  const acceptLanguageHeader = headersList.get("accept-language");
-  locale = acceptLanguageHeader?.split(",")[0].split("-")[0] || "en"; // Par défaut en 'en'
-
+export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming locale is supported
   if (!AVAILABLE_LOCALES.includes(locale)) {
     locale = "en";
   }
